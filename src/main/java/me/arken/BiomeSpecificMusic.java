5cb059887cbf3666649c55779a2c180e03700409
tripleX <vodelsup@gmail.com>
2021-12-18 17:36:10 +0100

    added registry through .minecraft/biosm folder

--- src/main/java/me/arken/BiomeSpecificMusic.java
+++ src/main/java/me/arken/BiomeSpecificMusic.java
@@ -4,7 +4,6 @@ import me.arken.events.SwitchBiomeCallback;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.sound.MusicSound;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.ActionResult;
@@ -13,14 +12,16 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
-import java.util.Map;
 
 public class BiomeSpecificMusic implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Map sounds = registerSounds();
+        HashMap sounds = registerSounds();
 
         SwitchBiomeCallback.EVENT.register((player, biome, soundManager) -> {
             player.sendChatMessage("New Biome: " + biome.getCategory());
@@ -29,7 +30,7 @@ public class BiomeSpecificMusic implements ModInitializer {
             SoundEvent sound = (SoundEvent) sounds.get(biome.getCategory());
             client.getMusicTracker().stop();
             if(sound != null) {
-                MusicSound musicSound = new MusicSound(sound, 20*5, 20*5, true);
+                MusicSound musicSound = new MusicSound(sound, 20, 20, true);
 
                 client.getMusicTracker().play(musicSound);
             }
@@ -38,18 +39,40 @@ public class BiomeSpecificMusic implements ModInitializer {
         });
     }
 
-    private Map registerSounds() {
-        File soundDir = FabricLoader.getInstance().getModContainer("biosm").get().getPath("assets/biosm/sounds").toFile();;
-        Map<Biome.Category, SoundEvent> sounds = new HashMap<>();
-            for(File file : soundDir.listFiles()) {
-                String fileName = file.getName().replaceAll(".ogg", "");
+    private HashMap registerSounds() {
+        File registryDir = FabricLoader.getInstance().getModContainer("biosm").get().getPath("assets/biosm/sounds").toFile();
+        File soundDir = new File(FabricLoader.getInstance().getGameDir().toFile().getPath()+"//biosm");
+        if(!soundDir.exists()) soundDir.mkdirs();
+        HashMap<Biome.Category, SoundEvent> sounds = new HashMap<>();
 
-                Identifier id = new Identifier("biosm:"+fileName);
-                SoundEvent event = new SoundEvent(id);
+        moveFiles(registryDir, soundDir);
 
-                sounds.put(Biome.Category.valueOf(fileName.toUpperCase()), event);
-                Registry.register(Registry.SOUND_EVENT, id, event);
-            }
+        //Register sounds
+        for(File file : soundDir.listFiles()) {
+            String fileName = file.getName().replaceAll(".ogg", "");
+
+            Identifier id = new Identifier("biosm:"+fileName);
+            SoundEvent event = new SoundEvent(id);
+
+            sounds.put(Biome.Category.valueOf(fileName.toUpperCase()), event);
+            Registry.register(Registry.SOUND_EVENT, id, event);
+        }
         return sounds;
     }
+
+    private void moveFiles(File registryDir, File soundDir) {
+        //Delete old files in registry
+        for(File file : registryDir.listFiles()) {
+            file.delete();
+        }
+        for(File file : soundDir.listFiles()) {
+            //Copy files to sounds folder
+            try {
+                Files.copy(file.toPath(), (new File(registryDir.getPath() + "//" + file.getName())).toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
